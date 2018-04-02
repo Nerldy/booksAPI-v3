037791c24556d65c8fa462621bec4a444f51b57b
Nerldy_ <arnodhis@gmail.com>
2018-04-02 19:35:07 +0300

    add PUT test case

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,6 +8,11 @@ class HelloBooksAPITestCase(unittest.TestCase):
 		response = tester.get('/books', content_type='application/json')
 		self.assertEqual(response.status_code, 200)
 
+	def test_put_works(self):
+		tester = app.test_client(self)
+		response = tester.put('/books', content_type='application/json')
+		self.assertEqual(response.status_code, 201)
+
 
 if __name__ == '__main__':
 	unittest.main()
