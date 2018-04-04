cbb5671a21b15d09f83db5d72bba38b7ed005c72
Nerldy_ <arnodhis@gmail.com>
2018-04-04 11:23:24 +0300

    replace book id with id so that it traces the correct object key

--- app/__init__.py
+++ app/__init__.py
@@ -28,10 +28,10 @@ def create_book():
 	req_data = request.get_json()  # turn json into Python objects
 
 	for book in books_collection:
-		if book["book_id"] == req_data["book_id"]:  # check if book with the same ISBN number exists
+		if book["id"] == req_data["id"]:  # check if book with the same IID number exists
 			return jsonify({"message": 'Book already exists'}), 400  # return this if book exists
 
-	book = Book(req_data['title'], req_data["book_id"])
+	book = Book(req_data['title'], req_data["isbn"])
 
 	for author in req_data['author']:
 		book.set_author(author)  # create author list
@@ -45,29 +45,27 @@ def create_book():
 def book_id_item(book_id):
 	if request.method == 'DELETE':
 		global books_collection
-		books_collection = list(filter(lambda x: x["book_id"] != book_id, books_collection))
+		books_collection = list(filter(lambda x: x["id"] != book_id, books_collection))
 		return jsonify({"message": "book deleted"})
 
 	if request.method == 'PUT':
 		req_data = request.get_json()
-		book = next(filter(lambda x: x["book_id"] == book_id, books_collection), None)  # search for book in collection
+		book = next(filter(lambda x: x["id"] == book_id, books_collection), None)  # search for book in collection
 
 		if book is None:
-			book = Book(req_data['title'], req_data["book_id"])
-
-			for author in req_data['author']:
-				book.set_author(author)  # create author list
-
-			books_collection.append(book.serialize())  # add book to dummy book list
-
-			return jsonify({"message": "Book has been created"})
+			return jsonify({"message": "Book does not exist"})
 		else:
 			book.update(req_data)
 			return jsonify({"message": "Book updated"})
 
 	else:
-		book = next(filter(lambda x: x["book_id"] == book_id, books_collection), None)
-		return jsonify({'book': book}), 200 if book else 404
+		# GET function will run here
+
+		book = next(filter(lambda x: x["id"] == book_id, books_collection), None)
+		if book is None:
+			return jsonify({"message": "Book does not exist"}), 404
+
+		return jsonify({'book': book}), 200
 
 
 @app.route('/users/books/<book_id>', methods=['POST'])
